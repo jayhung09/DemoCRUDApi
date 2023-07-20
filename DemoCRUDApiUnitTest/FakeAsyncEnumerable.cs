ce3d431adf99a58d39edbc329c1a9fb97faabded
jay <jack1000725@gmail.com>
2023-07-20 21:51:22 +0800

    Feat FakeAsyncEnumerable、FakeAsyncEnumerator

---
+++ DemoCRUDApiUnitTest/FakeAsyncEnumerable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCRUDApiUnitTest
+{
+    public class FakeAsyncEnumerable<T> : IAsyncEnumerable<T>
+    {
+        private readonly IEnumerable<T> _data;
+
+        public FakeAsyncEnumerable(IEnumerable<T> data)
+        {
+            _data = data;
+        }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            return new FakeAsyncEnumerator<T>(_data.GetEnumerator());
+        }
+    }
+
+    public class FakeAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _enumerator;
+
+        public FakeAsyncEnumerator(IEnumerator<T> enumerator)
+        {
+            _enumerator = enumerator;
+        }
+
+        public T Current => _enumerator.Current;
+
+        public ValueTask<bool> MoveNextAsync()
+        {
+            return new ValueTask<bool>(_enumerator.MoveNext());
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            _enumerator.Dispose();
+            return new ValueTask();
+        }
+    }
+}
